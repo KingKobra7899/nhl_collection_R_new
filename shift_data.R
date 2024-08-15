c6e37e99e4210d9efaea86b0565aef7bd3eefc35
KingKobra7899 <73727379+KingKobra7899@users.noreply.github.com>
2024-08-14 23:21:35 -0400

    Update shift_data.R

--- shift_data.R
+++ shift_data.R
@@ -1,22 +1,27 @@
 data("fenwick_data")
-data <- data[data$shot_outcome!="missed-shot", ]
-formula <- is_goal ~ xCoord + yCoord + distance + ns(angle, df = 3) + is_rebound + shotType
+data$angle <- data$angle * (pi / 180)
+
+base_formula <- is_goal ~ xCoord + yCoord + distance + angle + is_rebound + shotType
+cos_sin <- is_goal ~ xCoord + yCoord + distance + sin(angle) + cos(angle) + is_rebound + shotType
+cos_sin_abs <- is_goal ~ xCoord + yCoord + distance + sin(abs(angle)) + cos(abs(angle)) + is_rebound + shotType
+spline_model  <- is_goal ~ xCoord + yCoord + distance + ns(angle, df = 3) + is_rebound + shotType
+spline_model_abs  <- is_goal ~ xCoord + yCoord + distance + ns(abs(angle), df = 3) + is_rebound + shotType
+interaction <- is_goal ~ xCoord + yCoord + distance * angle + is_rebound + shotType
 train_control <- trainControl(
   method = "cv",        # Cross-validation
   number = 20,
   verboseIter = TRUE # Number of folds
 )
-glm.model <-train(formula,
+cos_sin.model <-train(cos_sin,
                   data = data,
                   method = "glm",
                   family = "binomial",
                   metric = "RMSE"
                   )
-summary(predict(glm.model, data))
-summary(glm.model$finalModel)
-
-game_id <- 2023021168
-team_id = 3
-model <- glm.model
-
-game <- get_rapm_scores(game_id, team_id, model)
+assess_xg <- function(model){
+  preds <- predict(model, data)
+  auc_score <- auc(data$is_goal, preds)
+  brier_score<- brier(data$is_goal, preds)
+  f1_score <- f1Score(data$is_goal, preds)
+  log_loss <- logLoss(data$is_goal, preds)
+}
