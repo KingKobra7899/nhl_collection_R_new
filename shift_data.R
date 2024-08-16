bafc09568ab6e8d0afd6cd4230e9e1c309d03379
KingKobra7899 <73727379+KingKobra7899@users.noreply.github.com>
2024-08-15 23:52:00 -0400

    Update shift_data.R

--- shift_data.R
+++ shift_data.R
@@ -1,27 +1,38 @@
 data("fenwick_data")
 data$angle <- data$angle * (pi / 180)
+data <- na.omit(data)
 
-base_formula <- is_goal ~ xCoord + yCoord + distance + angle + is_rebound + shotType
-cos_sin <- is_goal ~ xCoord + yCoord + distance + sin(angle) + cos(angle) + is_rebound + shotType
-cos_sin_abs <- is_goal ~ xCoord + yCoord + distance + sin(abs(angle)) + cos(abs(angle)) + is_rebound + shotType
-spline_model  <- is_goal ~ xCoord + yCoord + distance + ns(angle, df = 3) + is_rebound + shotType
-spline_model_abs  <- is_goal ~ xCoord + yCoord + distance + ns(abs(angle), df = 3) + is_rebound + shotType
-interaction <- is_goal ~ xCoord + yCoord + distance * angle + is_rebound + shotType
+cos_interaction <- is_goal ~ xCoord + yCoord + (cos(angle) * distance) + is_rebound + shotType
 train_control <- trainControl(
-  method = "cv",        # Cross-validation
-  number = 20,
   verboseIter = TRUE # Number of folds
 )
-cos_sin.model <-train(cos_sin,
-                  data = data,
-                  method = "glm",
-                  family = "binomial",
-                  metric = "RMSE"
-                  )
-assess_xg <- function(model){
+
+tune_grid <- expand.grid(alpha = c(0, 1), 
+                         lambda = 10^-5:0) 
+set.seed(123)
+split <- createDataPartition(data$is_goal, p = 0.8, list = F)
+train <- data[split,]
+test <- data[-split,]
+interaction_model_sin  <-train(cos_interaction,
+                           data = train,
+                           method = "gbm",
+                           preProcess = c("scale", "center"),
+                           #family = "binomial",
+                           metric = "RMSE",
+                           verbose = T
+)
+assess_xg <- function(model, data){
   preds <- predict(model, data)
+  print(summary(preds))
   auc_score <- auc(data$is_goal, preds)
   brier_score<- brier(data$is_goal, preds)
   f1_score <- f1Score(data$is_goal, preds)
   log_loss <- logLoss(data$is_goal, preds)
+  error <- rmse(data$is_goal, preds)
+  return(data.frame(auc = auc_score, brier = brier_score, f1 = f1_score, loss = log_loss, error = error))
 }
+
+assess_xg(interaction_model_sin, test)
+xg_model <- interaction_model_sin
+
+View(xg_model$results)
