ce0539ecffc5a82f245d2d5bc68961e66284cb04
KingKobra7899 <73727379+KingKobra7899@users.noreply.github.com>
2024-08-17 11:49:33 -0400

    Update shift_data.R

--- shift_data.R
+++ shift_data.R
@@ -4,25 +4,37 @@ data <- na.omit(data)
 
 cos_interaction <- is_goal ~ xCoord + yCoord + (cos(angle) * distance) + is_rebound + shotType
 train_control <- trainControl(
-  verboseIter = TRUE # Number of folds
+  verboseIter = TRUE,
+  classProbs = T
 )
 
 tune_grid <- expand.grid(alpha = c(0, 1), 
                          lambda = 10^-5:0) 
+data$is_goal <- as.factor(data$is_goal)
+levels(data$is_goal)[levels(data$is_goal)=="1"] = "yes"
+levels(data$is_goal)[levels(data$is_goal)=="0"] = "no"
 set.seed(123)
 split <- createDataPartition(data$is_goal, p = 0.8, list = F)
 train <- data[split,]
 test <- data[-split,]
-interaction_model_sin  <-train(cos_interaction,
-                           data = train,
-                           method = "gbm",
-                           preProcess = c("scale", "center"),
-                           #family = "binomial",
-                           metric = "RMSE",
-                           verbose = T
+cos_interaction <- is_goal ~ xCoord + yCoord + (cos(angle) * distance) + is_rebound + shotType
+interaction_model_sin <- train(
+  cos_interaction,
+  data = train,
+  method = "gbm",
+  preProcess = c("scale", "center"),
+  metric = "ROC",  # Use "ROC" for binary classification
+  trControl = trainControl(
+    method = "cv",       # Cross-validation
+    number = 5,          # Number of folds
+    classProbs = TRUE,   # Ensure probabilities are returned
+    summaryFunction = twoClassSummary,
+    verboseIter = T# For ROC metric
+  )
 )
 assess_xg <- function(model, data){
-  preds <- predict(model, data)
+  preds <- predict(model, data, type = "prob")$yes
+  data$is_goal <- as.integer(data$shot_outcome=="goal")
   print(summary(preds))
   auc_score <- auc(data$is_goal, preds)
   brier_score<- brier(data$is_goal, preds)
@@ -35,4 +47,6 @@ assess_xg <- function(model, data){
 assess_xg(interaction_model_sin, test)
 xg_model <- interaction_model_sin
 
+preds <- predict(interaction_model_sin, data, type = "prob")
+hist(preds$yes)
 View(xg_model$results)
